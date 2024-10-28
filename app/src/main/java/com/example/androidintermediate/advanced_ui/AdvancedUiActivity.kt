6c408ad0cb9233dcb8655146a5837b0f84d92ec0
Dimas Fahrul <dfpa.05@gmail.com>
2024-10-28 14:36:44 +0700

    add: canvas draw

--- app/src/main/java/com/example/androidintermediate/advanced_ui/AdvancedUiActivity.kt
+++ app/src/main/java/com/example/androidintermediate/advanced_ui/AdvancedUiActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidintermediate.advanced_ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
@@ -7,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.androidintermediate.R
+import com.example.androidintermediate.advanced_ui.canvas_draw.CanvasDrawActivity
 import com.example.androidintermediate.advanced_ui.custom_button.CustomButtonAndEditTextActivity
 import com.example.androidintermediate.advanced_ui.custom_view.CustomViewActivity
 import com.example.androidintermediate.databinding.ActivityAdvancedUiBinding
@@ -22,23 +24,20 @@ class AdvancedUiActivity : AppCompatActivity() {
 
         binding.btnMoveToCustomButton.setOnClickListener { onButtonClicked(it) }
         binding.btnMoveToCustomView.setOnClickListener { onButtonClicked(it) }
+        binding.btnMoveToCanvasDraw.setOnClickListener { onButtonClicked(it) }
 
         compatView()
     }
 
+    private fun setClass(cls: Class<*>): Intent = intent.setClass(this, cls)
+
     private fun onButtonClicked(it: View?) {
         when (it?.id) {
-            R.id.btn_move_to_custom_button -> {
-                val intent = intent
-                intent.setClass(this, CustomButtonAndEditTextActivity::class.java)
-                startActivity(intent)
-            }
-            
-            R.id.btn_move_to_custom_view -> {
-                val intent = intent
-                intent.setClass(this, CustomViewActivity::class.java)
-                startActivity(intent)
-            }
+            R.id.btn_move_to_custom_button -> startActivity(setClass(CustomButtonAndEditTextActivity::class.java))
+
+            R.id.btn_move_to_custom_view -> startActivity(setClass(CustomViewActivity::class.java))
+
+            R.id.btn_move_to_canvas_draw -> startActivity(setClass(CanvasDrawActivity::class.java))
         }
     }
 
